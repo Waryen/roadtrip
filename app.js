20b28dd945940140ed77b9244648d9447efba541
RayaneKaddoury <rayanekaddoury@gmail.com>
2022-06-14 22:44:53 +0200

    addCities baby

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
+let NameValue = document.getElementById("Name").value;
+let DistValue = document.getElementById("Distance").value;
+
+function addCities(name, distance) {
+    container = document.getElementById('container')
+
+    let div = document.createElement("div")
+    let pname = document.createElement("p")
+    let pdist = document.createElement("p")
+
+    let nameInput = document.createTextNode(NameValue)
+    let distInput = document.createTextNode(DistValue)
+
+    pname.appendChild(nameInput)
+    pdist.appendChild(distInput)
+
+    div.appendChild(pname)
+    div.appendChild(pdist)
+    
+    div.classList.add('city')
+
+    container.appendChild(div)
+
+}
+
 document.getElementById('form1').addEventListener("submit", (e) => {
     e.preventDefault()
-    let NameValue = document.getElementById("Name").value;
-    let DistValue = document.getElementById("Distance").value;
+
+    NameValue = document.getElementById("Name").value;
+    DistValue = document.getElementById("Distance").value;
 
     console.log(NameValue)
     console.log(DistValue)
 
+    addCities(NameValue, DistValue)
 })
